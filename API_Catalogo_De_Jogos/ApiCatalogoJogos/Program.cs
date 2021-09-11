aed7c87639ca7d7189347eaa8462c6af9eadfcc0
Pablo Luiz <80119424+devpabloluiz@users.noreply.github.com>
2021-09-11 13:56:10 -0300

    Add files via upload

---
+++ API_Catalogo_De_Jogos/ApiCatalogoJogos/Program.cs
@@ -0,0 +1,21 @@
+using ExemploApiCatalogoJogos;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace ApiCatalogoJogos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
